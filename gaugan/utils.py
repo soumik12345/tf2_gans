9cd90b79e2fd393d4be06884ac5c46fe52296ac8
Soumik Rakshit <soumik.rakshit@ignitarium.com>
2021-11-16 10:59:58 +0530

    added dataloader sanity tests

---
+++ gaugan/utils.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+
+def plot_results(images, titles, figure_size=(12, 12)):
+    fig = plt.figure(figsize=figure_size)
+    for i in range(len(images)):
+        fig.add_subplot(1, len(images), i + 1).set_title(titles[i])
+        _ = plt.imshow(images[i])
+        plt.axis("off")
+    plt.show()
