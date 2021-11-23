9b10ea427f12ed0da284ff2bfa317a1ba9b43507
Sayak Paul <spsayakpaul@gmail.com>
2021-11-23 19:17:34 +0530

    fix: test for data loader

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import unittest
 import tensorflow as tf
 
 from gaugan import PairedTranslationDataLoader, models
+import os
 
 
 class PairedTranslationDataLoaderTester(unittest.TestCase):
@@ -9,8 +10,13 @@ class PairedTranslationDataLoaderTester(unittest.TestCase):
         super().__init__(methodName=methodName)
 
     def test_facades(self):
+        os.system(
+            "gdown https://drive.google.com/uc?id=1zs2RPZkLeB5QDRVQtpqUtMwepR_Qfufp"
+        )
+        os.system("unzip -q facades.zip")
         data_loader = PairedTranslationDataLoader()
         train_dataset, val_dataset = data_loader.get_datasets(dataset_path="./facades/")
+
         image, segmentation_map, one_hot_encoded_labels = next(iter(train_dataset))
         assert image.shape == (16, 256, 256, 3)
         assert segmentation_map.shape == (16, 256, 256, 3)
