6897d6da76fb8c097e4a4305db14d3950b9a9aec
soumik12345 <19soumik.rakshit96@gmail.com>
2022-01-01 17:21:01 +0530

    added residual block + downsample block

---
+++ gaugan/models/blocks.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras import layers, initializers
+import tensorflow_addons as tfa
+
+from .spade import SPADE
+
+
+class ResBlock(layers.Layer):
+    def __init__(self, filters, **kwargs):
+        super().__init__(**kwargs)
+        self.filters = filters
+
+    def build(self, input_shape):
+        input_filter = input_shape[-1]
+        self.spade_1 = SPADE(input_filter)
+        self.spade_2 = SPADE(self.filters)
+        self.conv_1 = layers.Conv2D(self.filters, 3, padding="same")
+        self.conv_2 = layers.Conv2D(self.filters, 3, padding="same")
+        self.learned_skip = False
+
+        if self.filters != input_filter:
+            self.learned_skip = True
+            self.spade_3 = SPADE(input_filter)
+            self.conv_3 = layers.Conv2D(self.filters, 3, padding="same")
+
+    def call(self, input_tensor, mask):
+        x = self.spade_1(input_tensor, mask)
+        x = self.conv_1(tf.nn.leaky_relu(x, 0.2))
+        x = self.spade_2(x, mask)
+        x = self.conv_2(tf.nn.leaky_relu(x, 0.2))
+        skip = (
+            self.conv_3(tf.nn.leaky_relu(self.spade_3(input_tensor, mask), 0.2))
+            if self.learned_skip
+            else input_tensor
+        )
+        return skip + x
+
+
+def downsample(
+    channels,
+    kernels,
+    strides=2,
+    apply_norm=True,
+    apply_activation=True,
+    apply_dropout=False,
+):
+    block = Sequential()
+    block.add(
+        layers.Conv2D(
+            channels,
+            kernels,
+            strides=strides,
+            padding="same",
+            use_bias=False,
+            kernel_initializer=initializers.GlorotNormal(),
+        )
+    )
+    if apply_norm:
+        block.add(tfa.layers.InstanceNormalization())
+    if apply_activation:
+        block.add(layers.LeakyReLU(0.2))
+    if apply_dropout:
+        block.add(layers.Dropout(0.5))
+    return block
